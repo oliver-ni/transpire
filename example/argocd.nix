48c8a04d9baa5beaedad18a32e19df8269040de5
Oliver Ni <oliverni@berkeley.edu>
2024-05-17 14:55:34 -0700

    feat: add example of goal

---
+++ example/argocd.nix
@@ -0,0 +1,22 @@
+{ lib, transpire, ... }:
+
+{
+  namespaces.argocd = {
+    helmReleases = {
+      argocd = {
+        chart = transpire.fetchFromHelm {
+          repo = "https://argoproj.github.io/argo-helm";
+          name = "argo-cd";
+          version = "6.9.2";
+          sha256 = "9gwn1+8l40UlsD3UoB6rVrOpF/cVjjJGk8f6WZqllkE=";
+        };
+
+        values = { };
+      };
+    };
+
+    objects = {
+      "apps/v1".Deployment.test = { };
+    };
+  };
+}
