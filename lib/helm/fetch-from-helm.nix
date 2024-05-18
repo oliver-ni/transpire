6fcf17357fe1648d285030aad9b51b0531df8e7e
Oliver Ni <oliverni@berkeley.edu>
2024-05-17 18:11:01 -0700

    feat: add helm chart fetcher

---
+++ lib/helm/fetch-from-helm.nix
@@ -0,0 +1,25 @@
+{ lib, stdenvNoCC, kubernetes-helm }:
+
+lib.makeOverridable (
+  { repo
+  , name
+  , version
+  , sha256 ? lib.fakeSha256
+  }:
+
+  stdenvNoCC.mkDerivation {
+    pname = "helm-${name}";
+    version = version;
+    nativeBuildInputs = [ kubernetes-helm ];
+
+    buildCommand = ''
+      export HELM_CACHE_HOME="$TMP/.cache"
+      helm pull --repo ${repo} ${name} --untar
+      mv ${name} $out
+    '';
+
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = sha256;
+  }
+)
